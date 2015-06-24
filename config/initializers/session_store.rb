f0041b6b7c628c73a7e1ccf3a8345aa9412f9e0f
Stefan Wallin <stefan.w@festiz.com>
2015-06-24 22:11:24 +0200

    Add domain back into session-store.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,6 @@
 # Be sure to restart your server when you modify this file.
 Avatar::Application.config.session_store(
   :cookie_store,
-  key: '_avatar_session'
+  key: '_avatar_session',
+  domain: ENV['DOMAIN']
 )
